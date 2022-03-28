44969c9a4fe0b72dfcf3a577ab96cb47b61e730c
Rafael Farias <rafaelfariasprogramador@gmail.com>
2022-03-28 15:28:40 -0300

    Novas classes: Piece.java e Board.java

--- src/App.java
+++ src/App.java
@@ -1,7 +1,8 @@
+import boardgame.Board;
 import boardgame.Position;
 
 public class App {
     public static void main(String[] args) throws Exception {
-    
+        Board board = new Board(8, 8);
     }
 }
