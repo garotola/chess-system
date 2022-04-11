adde41fa569a9183bf3d3bcaa5523ad3d126e275
Rafael Farias <rafaelfariasprogramador@gmail.com>
2022-04-11 13:06:21 -0300

    Criando a peça peão e adicionando no tabuleiro de xadrez

---
+++ src/chess/pieces/Pawn.java
@@ -0,0 +1,51 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Pawn extends ChessPiece{
+
+    public Pawn(Board board, Color color) {
+        super(board, color);
+    }
+    @Override
+    public String toString(){
+        return "P";
+    }
+    
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        Position p = new Position(0, 0);
+        if(getColor() == Color.WHITE){ //Peças brancas
+            // Movimento pra cima(uma casa)
+            p.setValues(position.getRow() - 1, position.getColumn());
+            if(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) mat[p.getRow()][p.getColumn()] = true;
+            //Movimento pra cima(duas casas)
+            p.setValues(position.getRow() - 2, position.getColumn());
+            Position p2 = new Position(position.getRow() - 1, position.getColumn());
+            if(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) mat[p.getRow()][p.getColumn()] = true;
+            //Movimento para captura de peças
+            p.setValues(position.getRow() - 1, position.getColumn() - 1); // Diagonal esquerda
+            if(getBoard().positionExists(p) && isThereOpponentPiece(p)) mat[p.getRow()][p.getColumn()] = true;
+            p.setValues(position.getRow() - 1, position.getColumn() + 1); // Diagonal direita
+            if(getBoard().positionExists(p) && isThereOpponentPiece(p)) mat[p.getRow()][p.getColumn()] = true;
+        }else{ //Peças pretas
+            // Movimento pra cima(uma casa)
+            p.setValues(position.getRow() + 1, position.getColumn());
+            if(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) mat[p.getRow()][p.getColumn()] = true;
+            //Movimento pra cima(duas casas)
+            p.setValues(position.getRow() + 2, position.getColumn());
+            Position p2 = new Position(position.getRow() + 1, position.getColumn());
+            if(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) mat[p.getRow()][p.getColumn()] = true;
+            //Movimento para captura de peças
+            p.setValues(position.getRow() + 1, position.getColumn() - 1); // Diagonal esquerda
+            if(getBoard().positionExists(p) && isThereOpponentPiece(p)) mat[p.getRow()][p.getColumn()] = true;
+            p.setValues(position.getRow() + 1, position.getColumn() + 1); // Diagonal direita
+            if(getBoard().positionExists(p) && isThereOpponentPiece(p)) mat[p.getRow()][p.getColumn()] = true;
+        }
+        return mat;
+    }
+}
